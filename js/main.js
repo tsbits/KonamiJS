c53b3fed4202958687b639171f87b887c3cfdb18
tsbits <contact@tsbits.net>
2022-07-16 02:07:18 +0200

    K.I.S.S

--- js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-	konamiInstance = new Konami();
-
-	konamiInstance.addEventListener('konami-detected', (e) => {
-		console.log(`Konami Code has been triggered ${e.detail.times} times.`);
-	});
-});
